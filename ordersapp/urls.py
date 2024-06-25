5b9c2e436ea3f9f7b40c227cf49161dd822807c7
astronom <chasovnikov.ar17@physics.msu.ru>
2024-06-25 17:00:37 +0300

    vrodenorm

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from . import views
+from .views import Last
 
 urlpatterns = [
- path('', views.index, name='index'),
+ path('last/<int:clientid>/<int:days>/', Last.as_view(), name='last'),
 ]
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from datetime import date, timedelta
 from django.http import HttpResponse
+from django.views import View
+from django.shortcuts import render
+from .models import Product, Order
+
+
 
 def index(request):
     return HttpResponse("Welcome to our shop!")
+
+
+class Last(View):
+    def get(self, request, clientid, days):
+        orders = Order.objects.filter(client_id=clientid)
+        pokupky = []
+        for i in range (len(orders)):
+            if orders[i].making_date >= date.today()-timedelta(days):    
+                pokupky.append(Product.objects.filter(id = orders[i].product_id.pk).first().name)
+        context = {'spisok':pokupky}
+        return render(request, 'ordersapp/last.html', context)
