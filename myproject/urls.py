d39ac2b68c5d7ecc0dc0a6739f97c5e93d7a9cde
astronom <chasovnikov.ar17@physics.msu.ru>
2024-07-05 18:25:18 +0300

    finale

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('random/', include('myapp.urls')),
     path('shop/', include('ordersapp.urls')),
+    #path('__debug__/', include("debug_toolbar.urls")),
 ]
